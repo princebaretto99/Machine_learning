6ec960875e67d4e066dfb8693def0a6197cc9fdb
princebaretto99 <43331485+princebaretto99@users.noreply.github.com>
2019-04-14 13:47:51 +0530

    Update Heart_disease_diagnosis.py

--- Heart_disease_diagnosis.py
+++ Heart_disease_diagnosis.py
@@ -33,3 +33,31 @@ prediction=clf.predict(X_test)
 from sklearn.metrics import accuracy_score
 score=accuracy_score(prediction,y_test)
 print(score)
+
+import pickle
+model=pickle.dumps(clf)
+pickle_out=pickle.loads(model)
+
+age=int(input('Please enter your age: '))
+sex=int(input('1: Male  0: Female: '))
+cp_type = int(input('chest pain type||1:typical angina||2:atypical angina||3:non-anginal pain||4:asymptomatic: '))
+rest_bp = int(input('Resting Blood Pressure: '))
+chol=int(input('Serum Cholesterol: '))
+fast_bs=int(input('Fasting Blood Sugar: '))
+restecg= int(input('resting electrocardiographic results( Value 0: normal ,Value 1: having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)'))
+maxhr=int(input('Maximum Heart Rate: '))
+exang=int(input('exercise induced angina(1:Yes,0:No): '))
+oldpeak=float(input('Old Peak: '))
+slope=int(input('the slope of the peak exercise ST segment 1:upsloping 2:Flat 3: downsloping: '))
+ca=int(input('ca:  number of major vessels (0-3) colored by flourosopy: '))
+thal=int(input('thal(3 = normal; 6 = fixed defect; 7 = reversable defect): '))
+
+data1=[[age,sex,cp_type,rest_bp,chol,fast_bs,restecg,maxhr,exang,oldpeak,slope,ca,thal]]
+df=pd.DataFrame(data1)
+
+resultant=pickle_out.predict(df)
+
+if resultant==1:
+  print('You need to visit the doctor because you have a Heart Disease')
+else:
+  print('Dont Worry its just a minor chest pain!! Your Lucky ;)')
